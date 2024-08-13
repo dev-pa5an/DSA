9cb7d39080694b140705690824eefe0f7dd9aee9
dev-pa5an <pasanfernando24@gmail.com>
2024-08-13 22:41:16 +0530

    Hidden behavior of the Object class

--- Java 17/SuperKeyword.java
+++ Java 17/SuperKeyword.java
@@ -1,6 +1,9 @@
-class A{
+//every class in java extends Object class
+//It is the reason even you don't specify that super() is called in the constructor
+
+class A extends Object{
     public A(){
-        super();
+        super(); //this super represents the constructor of the Object class
     }
     public A(int n){
         System.out.println("In A int");
@@ -9,8 +12,8 @@ class A{
 class B extends A{
     //always the super class's constructor will be called
     public B(){
-        super();
-        System.out.println("In A");
+        super(4); //this super represents the constructor of the A class int n constructor
+        System.out.println("In B");
     }
     public B(int n){
         super(n);
@@ -21,7 +24,7 @@ class B extends A{
 public class SuperKeyword{
     
     public static void main(String[] args) {
-        B obj = new B(3);
+        B obj = new B();
 
     }
 }
