d4057576b825335634e554e9339457540415f5c6
dev-pa5an <pasanfernando24@gmail.com>
2024-09-16 11:32:18 +0530

    hashtable and hashmap difference

--- Java 17/Collection/Maps.java
+++ Java 17/Collection/Maps.java
@@ -1,11 +1,19 @@
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 
 public class Maps{
     public static void main(String[] args) {
+
         Map<String, Integer> students = new HashMap<>();
         students.put("Pasan", 10);
         students.put("Yasith", 9);
+
+        //hashtable is suitable to work with multiple threads.
+        //hashtable is synchronized
+        Map<String, Integer> fruits = new Hashtable<>();
+        fruits.put("promegranante", 4);
+        fruits.put("orange", 6);
 
 
         for(String key : students.keySet()){
