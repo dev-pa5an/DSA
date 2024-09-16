e292c8aa23b614486613a56534a1ce266af24fb0
dev-pa5an <pasanfernando24@gmail.com>
2024-09-16 16:30:52 +0530

    soring with comparable

--- Java 17/Collection/Sorting.java
+++ Java 17/Collection/Sorting.java
@@ -20,5 +20,79 @@ public class Sorting{
 
         Collections.sort(nums, comparator); //used TimSort algorithm
         System.out.println(nums);
+
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Pasan", 25));
+        students.add(new Student("Thehassa", 22));
+        students.add(new Student("Hansi", 21));
+
+        Comparator<Student> comStud = (Comparator<Student>) (Student s1, Student s2) -> {
+            if(s1.age > s2.age)
+                return 1;
+            else    
+                return -1;
+        };
+
+        Collections.sort(students, comStud);
+        System.out.println(students);
+
+        List<StudentWithComparable> students2 = new ArrayList<>();
+        students2.add(new StudentWithComparable("Pasan", 25));
+        students2.add(new StudentWithComparable("Thehassa", 22));
+        students2.add(new StudentWithComparable("Hansi", 21));
+
+        Collections.sort(students2);
+        System.out.println(students2);
     }
+}
+
+class Student{
+    String name;
+    int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Student{");
+        sb.append("name=").append(name);
+        sb.append(", age=").append(age);
+        sb.append('}');
+        return sb.toString();
+    }
+
+}
+
+class StudentWithComparable implements Comparable<StudentWithComparable>{
+    
+    String name;
+    int age;
+
+    public StudentWithComparable(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(StudentWithComparable that) {
+        if(this.age > that.age)
+            return 1;
+        else
+            return -1;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("StudentWithComparable{");
+        sb.append("name=").append(name);
+        sb.append(", age=").append(age);
+        sb.append('}');
+        return sb.toString();
+    }
+
 }
