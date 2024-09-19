c8e5a6c9f2e941dfd79169b94f8162fb117f6641
dev-pa5an <pasanfernando24@gmail.com>
2024-09-19 21:22:39 +0530

    Stream example

--- Java 17/Collection/StreamsDemo.java
+++ Java 17/Collection/StreamsDemo.java
@@ -1,18 +1,25 @@
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class StreamsDemo{
     public static void main(String[] args) {
         
         List<Integer> nums = Arrays.asList(4,6,3,1,9,8);
         
-        Stream<Integer> s1 = nums.stream();
+        // Stream<Integer> s1 = nums.stream();
+        // Stream<Integer> s2 = s1.filter(n -> n%2==0);
+        // Stream<Integer> s3 = s2.map(n -> n*2);
+        // int result = s3.reduce(0, (c,e) -> c+e);
 
-        Stream<Integer> s2 = s1.filter(n -> n%2==0);
+        int result = nums.stream()
+                            .filter(n -> n%2==0)
+                            .map(n -> n*2)
+                            .reduce(0, (c,e) -> c+e);
 
-        s2.forEach(n -> System.out.println(n));
+        // s2.forEach(n -> System.out.println(n));
+        // s3.forEach(n -> System.out.println(n));
+        System.out.println(result);
 
     }
 }
