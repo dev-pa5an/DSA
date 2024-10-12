256e13970d64ef31d5b81a80eee0a1d4283101d6
dev-pa5an <pasanfernando24@gmail.com>
2024-10-12 08:32:18 +0530

    Useful methods implementation for Singly Linked List Class

--- Java 17/API/Linked List/SinglyLinkedList.java
+++ Java 17/API/Linked List/SinglyLinkedList.java
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 public class SinglyLinkedList{
 
     private ListNode head;
@@ -11,8 +17,8 @@ public class SinglyLinkedList{
             this.next = null;
         }
     } 
-    public void showSSL(SinglyLinkedList ssl){
-        ListNode current = ssl.head;
+    void showSSL(){
+        ListNode current = head;
         while (current != null){
             System.out.print(current.data + " --> ");
             current = current.next;
@@ -22,9 +28,12 @@ public class SinglyLinkedList{
         }
     }
 
-    public int findLenght(SinglyLinkedList ssl){
+    int findLenght(){
+        if (head == null){
+            return 0;
+        }
         int count = 0;
-        ListNode current = ssl.head;
+        ListNode current = head;
         while (current != null){
             count++;
             current = current.next;
@@ -32,7 +41,77 @@ public class SinglyLinkedList{
         
         return count;
     }
+    void insertFirst(int data){
+        ListNode newNode = new ListNode(data);
+        newNode.next = head;
+        head = newNode;
+    }
+    void insertLast(int data){
+        ListNode newNode = new ListNode(data);
+        if (head == null){
+            head = newNode;
+            return;
+        }
+        ListNode currentNode = head;
+        while (currentNode.next != null){
+            currentNode = currentNode.next;
+        }
+        currentNode.next = newNode;
+    }
+    void insertAt(int data, int position){
+        ListNode newNode = new ListNode(data);
+        if (position == 1){
+            newNode.next = head;
+            head = newNode;
+        } else {
+            ListNode current = head;
+            for (int i = 1; i < position - 1; i++){
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+    }
+    void deleteFirst(){
+        if (head == null) return;
+        head = head.next;
+    }
+    public void deleteLast(){
+        if (head == null) return;
+        if (head.next == null){
+            head = null;
+            return;
+        }
+        ListNode current = head;
+        ListNode previous = head;
+        while (current.next != null){
+            previous = current;
+            current = current.next;
+        }
+        previous.next = null;  
+    }
+    public void deleteAt(int position){
+        if (position == 1){
+            head = head.next;
+            return;
+        }
+        ListNode current = head;
+        ListNode previous = head;
+        for (int i = 1; i < position; i++){
+            previous = current;
+            current = current.next;
+        }
+        previous.next = current.next;
 
+    }
+    public boolean exists(int searchkey){
+        ListNode current = head;
+        while (current != null){
+            if (current.data == searchkey) return true;
+            current = current.next;
+        }
+        return false;
+    }
     public static void main(String[] args) {
 
         SinglyLinkedList sll = new SinglyLinkedList();
@@ -46,8 +125,45 @@ public class SinglyLinkedList{
         second.next = third;    // 10 --> 5 --> 8
         third.next = fourth;    // 10 --> 5 --> 8 --> 16 --> null
 
-        sll.showSSL(sll);
-        System.out.println(sll.findLenght(sll));
+        sll.showSSL();
+        // System.out.println(sll.findLenght());
+        
+        //insert a new node at the beginning of a singly linked list
+        System.out.println("adding a new node at the beginning");
+        sll.insertFirst(10);
+        sll.showSSL();
+        System.out.println();
+        //insert a new node at a given position
+        System.out.println("adding a new node at a given position");
+        sll.insertAt(80, 4);
+        sll.showSSL();
+
+        System.out.println("deleting the first node");
+        //delete the first node
+        sll.deleteFirst();
+        sll.showSSL();
+
+        System.out.println("deleting the last node");
+        //delete the last node
+        sll.deleteLast();
+        sll.showSSL();
+
+        //delete a node at a given position
+        int position = 1;
+        System.out.println("delting the node at position" + position);
+        sll.deleteAt(position);
+        sll.showSSL();
+
+        //search for a value
+        int searchkey = 3;
+        if (sll.exists(searchkey)){
+            System.out.print(searchkey + " exists in the singly linked list ");
+            sll.showSSL();
+        } else {
+            System.out.print(searchkey + " does not exist in the singly linked list ");
+            sll.showSSL();
+        }
+        
 
     }
 }
