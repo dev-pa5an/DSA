849fd480b58d1b167107d4fe73582e217da04fe5
dev-pa5an <pasanfernando24@gmail.com>
2024-10-15 21:39:15 +0530

    insert first, delete first, insert last method implementation

--- Java 17/API/Lists/DoublyLinkedList.java
+++ Java 17/API/Lists/DoublyLinkedList.java
@@ -1,12 +1,10 @@
-
-
 public class DoublyLinkedList{
     private ListNode head;
     private ListNode tail;
-    private int length;
+    private final int length;
 
     private static class ListNode{
-        private int data;
+        private final int data;
         private ListNode previous;
         private ListNode next;
 
@@ -40,27 +38,67 @@ public class DoublyLinkedList{
         }
         if(temp == null) System.out.println("null");
     }
+    public void insertFirst(int data){
+        ListNode newNode = new ListNode(data);
+        if (this.head == null){
+            tail = newNode;
+        } else{
+            newNode.next = head;
+            head.previous = newNode;     
+        }
+        head = newNode;
+    }
+    public void insertLast(int data){
+        ListNode newNode = new ListNode(data);
+        if (this.head == null){
+            head = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.previous = tail;
+        }
+        tail = newNode;
+    }
+    public void deleteFirst(){
+        try {
+            ListNode temp = head.next;
+            head.next.previous = null;
+            head = temp;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
     public static void main(String[] args) {
         DoublyLinkedList dll = new DoublyLinkedList();
         
-        ListNode n1 = new ListNode(1);
-        ListNode n2 = new ListNode(2);
-        ListNode n3 = new ListNode(3);
-        ListNode n4 = new ListNode(4);
+        // ListNode n1 = new ListNode(2);
+        // ListNode n2 = new ListNode(3);
+        // ListNode n3 = new ListNode(4);
+        // ListNode n4 = new ListNode(5);
 
-        //forward
-        n1.next = n2;
-        n2.next = n3;
-        n3.next = n4;
-        //backward
-        n4.previous = n3;
-        n3.previous = n2;
-        n2.previous = n1;
-        //assigning head & tail
-        dll.head = n1;
-        dll.tail = n4;
+        // //forward
+        // n1.next = n2;
+        // n2.next = n3;
+        // n3.next = n4;
+        // //backward
+        // n4.previous = n3;
+        // n3.previous = n2;
+        // n2.previous = n1;
+        // //assigning head & tail
+        // dll.head = n1;
+        // dll.tail = n4;
 
         dll.displayForward();
         dll.displayBackward();
+
+        //insert a node in the beginning
+        dll.insertFirst(1);
+        dll.displayForward();
+        dll.displayBackward();
+
+        //delete the first node
+        dll.deleteFirst();
+        dll.displayForward();
+        dll.insertFirst(2);
+        dll.displayForward();
     }
 }
