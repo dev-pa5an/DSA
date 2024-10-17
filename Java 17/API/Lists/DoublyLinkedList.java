5a0d5bd7e802f1463da96d123cab9b07f5e3e482
dev-pa5an <pasanfernando24@gmail.com>
2024-10-17 23:36:37 +0530

    delete the last node

--- Java 17/API/Lists/DoublyLinkedList.java
+++ Java 17/API/Lists/DoublyLinkedList.java
@@ -67,25 +67,30 @@ public class DoublyLinkedList{
             throw e;
         }
     }
+    public void deleteLast(){
+        ListNode temp = tail;
+        tail.previous.next = null;
+        tail = temp;
+    }
     public static void main(String[] args) {
         DoublyLinkedList dll = new DoublyLinkedList();
         
-        // ListNode n1 = new ListNode(2);
-        // ListNode n2 = new ListNode(3);
-        // ListNode n3 = new ListNode(4);
-        // ListNode n4 = new ListNode(5);
+        ListNode n1 = new ListNode(2);
+        ListNode n2 = new ListNode(3);
+        ListNode n3 = new ListNode(4);
+        ListNode n4 = new ListNode(5);
 
-        // //forward
-        // n1.next = n2;
-        // n2.next = n3;
-        // n3.next = n4;
-        // //backward
-        // n4.previous = n3;
-        // n3.previous = n2;
-        // n2.previous = n1;
-        // //assigning head & tail
-        // dll.head = n1;
-        // dll.tail = n4;
+        //forward
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        //backward
+        n4.previous = n3;
+        n3.previous = n2;
+        n2.previous = n1;
+        //assigning head & tail
+        dll.head = n1;
+        dll.tail = n4;
 
         dll.displayForward();
         dll.displayBackward();
@@ -99,6 +104,10 @@ public class DoublyLinkedList{
         dll.deleteFirst();
         dll.displayForward();
         dll.insertFirst(2);
+        dll.displayForward();
+
+        //delete the last node
+        dll.deleteLast();
         dll.displayForward();
     }
 }
