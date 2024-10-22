118c5e15866682ac2be08882a19d28b3455855d6
dev-pa5an <pasanfernando24@gmail.com>
2024-10-22 22:01:10 +0530

    recursive pre-order traversal

--- Java 17/API/Trees/BinaryTree.java
+++ Java 17/API/Trees/BinaryTree.java
@@ -16,17 +16,29 @@ public class BinaryTree{
         this.root = null;
     }
     public void createBinaryTree(){
-        TreeNode n1 = new TreeNode(1);
+        TreeNode n1 = new TreeNode(9);
         TreeNode n2 = new TreeNode(2);
         TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
         
         root = n1;
         n1.left = n2;
         n1.right = n3;
+        n2.left = n4;
+        
+    }
+    public void preOrderTraversal(TreeNode root){
+        if (root == null){
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrderTraversal(root.left);
+        preOrderTraversal(root.right);
     }
     public static void main(String[] args) {
         BinaryTree bt = new BinaryTree();
         bt.createBinaryTree();
+        bt.preOrderTraversal(bt.root);
         
     }
 }
