6ef1544bb87f69e85e480dddc4d4ce86ad0c4c75
dev-pa5an <pasanfernando24@gmail.com>
2024-10-25 23:27:20 +0530

    in-order, pre-order & level-order traversal in binary tree

--- Java 17/API/Trees/BinaryTree.java
+++ Java 17/API/Trees/BinaryTree.java
@@ -1,4 +1,6 @@
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class BinaryTree{
@@ -19,15 +21,27 @@ public class BinaryTree{
         this.root = null;
     }
     public void createBinaryTree(){
-        TreeNode n1 = new TreeNode(9);
+        TreeNode n1 = new TreeNode(1);
         TreeNode n2 = new TreeNode(2);
         TreeNode n3 = new TreeNode(3);
         TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n7 = new TreeNode(7);
+        TreeNode n8 = new TreeNode(8);
+        TreeNode n9 = new TreeNode(9);
+        TreeNode n10 = new TreeNode(10);
         
         root = n1;
         n1.left = n2;
         n1.right = n3;
         n2.left = n4;
+        n2.right = n5;
+        n3.left = n6;
+        n3.right = n7;
+        n4.left = n8;
+        n6.left = n9;
+        n6.right = n10;
         
     }
     //pre-order traversal recursive approach
@@ -51,10 +65,39 @@ public class BinaryTree{
             if (temp.left != null) stack.push(temp.left);
         }
     }
+    //in-order traversal recursive approach
+    public void inOrder(TreeNode root){
+        if (root != null){
+            inOrder(root.left);
+            System.out.print(root.data + " ");
+            inOrder(root.right);
+        }
+    }
+    //level order traversal
+    public void levelOrder(TreeNode root){
+        if (root != null){
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            TreeNode current = root;
+            while (!queue.isEmpty()){
+                current = queue.poll();
+                if (current != null){
+                    System.out.print(current.data + " ");
+                    queue.offer(current.left);
+                    queue.offer(current.right);
+                } 
+            }
+        }
+    }
     public static void main(String[] args) {
         BinaryTree bt = new BinaryTree();
         bt.createBinaryTree();
         bt.preOrderTraversal(bt.root);
+        System.out.println();
+        bt.inOrder(bt.root);
+        System.out.println();
+        bt.levelOrder(bt.root);
+        System.out.println();
         
     }
 }
