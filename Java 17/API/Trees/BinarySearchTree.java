0704383ad2bb82aea9f70ef5697221f9d4ac9e90
dev-pa5an <pasanfernando24@gmail.com>
2024-10-27 13:00:37 +0530

    insert, inOrder, isValid methods implementation

--- Java 17/API/Trees/BinarySearchTree.java
+++ Java 17/API/Trees/BinarySearchTree.java
@@ -27,7 +27,16 @@ public class BinarySearchTree{
         else root.right = insert(root.right, value);
         return root;
     }
-    public void levelOrderTraversal(TreeNode root){
+    private void inOrder(TreeNode root){
+        if (root == null) return;
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+    public void inOrder(){
+        inOrder(root);
+    }
+    private void levelOrderTraversal(TreeNode root){
         if (root == null) return;
         Queue<TreeNode> q = new LinkedList<>();
         TreeNode current = root;
@@ -40,12 +49,44 @@ public class BinarySearchTree{
 
         }
     }
+    public void levelOrderTraversal(){
+        levelOrderTraversal(root);
+    }
+    public TreeNode search(int key){
+        return search(root, key);
+    }
+    private TreeNode search(TreeNode root, int key){
+        if (root == null || root.data == key) return root;
+        if (key < root.data) return search(root.left, key);
+        else return search(root.right, key);
+    }
+    public boolean isValid(){
+        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    private boolean isValid(TreeNode root, long min, long max){
+        if (root == null) return true;
+        if (root.data < min || root.data > max) return false;
+        boolean left = isValid(root.left, min, root.data);
+        if (left){
+            boolean right = isValid(root.right, root.data, max);
+            return right;
+        }
+        return false;
+    }
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree();
         bst.insert(4);
-        bst.insert(bst.root, 2);
-        bst.insert(bst.root, 5);
-        bst.levelOrderTraversal(bst.root);
-        
+        bst.insert(2);
+        bst.insert(5);
+        System.out.print("This is level-order traversal: ");
+        bst.levelOrderTraversal();
+        System.out.println();
+        System.out.print("This is in-rder traversal: ");
+        bst.inOrder();
+        System.out.println();
+        int key = 3;
+        if (bst.search(3) != null) System.out.println(key + " is found");
+        else System.out.println(key + " is not found");
+        if (bst.isValid()) System.out.println("this is a valid bst");
     }
 }
